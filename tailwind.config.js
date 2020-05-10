17c098113d4c775493a896ce35b3b8dbed40a639
givemefish <givemefish@gmail.com>
2020-05-10 13:00:16 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  purge: [],
+  theme: {
+    spacing: {
+      0: '0',
+      1: '.25rem',
+      2: '.5rem',
+      3: '.75rem',
+      4: '1rem'
+    },
+    padding: {
+      0: '0',
+      1: '.25rem',
+      2: '.5rem',
+      3: '.75rem',
+      4: '1rem'
+    },
+    margin: {
+      0: '0',
+      1: '.25rem',
+      2: '.5rem',
+      3: '.75rem',
+      4: '1rem'
+    },
+    textColor: {
+      'accent': '#1984c8',
+      'base': '#f3f3f4',
+      'error': '#ed92800',
+      'warning': '#ff9800',
+      'success': '#3ea44e',
+      'info': '#2498bc',
+      'transparent': 'transparent',
+      'curent': 'currentColor',
+      'black': '#000',
+      'white': '#fff',
+      'gray': '#515967'
+    },
+    backgroundColor: {
+      'accent': '#1984c8',
+      'base': '#f3f3f4',
+      'error': '#ed92800',
+      'warning': '#ff9800',
+      'success': '#3ea44e',
+      'info': '#2498bc',
+      'transparent': 'transparent',
+      'curent': 'currentColor',
+      'black': '#000',
+      'white': '#fff',
+      'gray': '#515967'
+    },
+    borderColor: {
+      'accent': '#1984c8',
+      'base': '#f3f3f4',
+      'error': '#ed92800',
+      'warning': '#ff9800',
+      'success': '#3ea44e',
+      'info': '#2498bc',
+      'transparent': 'transparent',
+      'curent': 'currentColor',
+      'black': '#000',
+      'white': '#fff',
+      'gray': '#515967'
+    }
+  },
+  variants: {},
+  corePlugins: {
+    accessibility: false,
+    backgroundAttachment: false,
+    backgroundPosition: false,
+    backgroundRepeat: false,
+    backgroundSize: false,
+    boxSizing: false,
+    clear: false,
+    float: false,
+    inset: false,
+    objectFit: false,
+    objectPosition: false,
+    pointerEvents: false,
+    rotate: false,
+    scale: false,
+    skew: false,
+    transformOrigin: false,
+    transitionProperty: false,
+    transitionDuration: false,
+    transitionTimingFunction: false,
+    transitionDelay: false,
+    translate: false,
+    zIndex: false
+  },
+  plugins: []
+}
